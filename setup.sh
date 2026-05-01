ef1100fd5bf53b8c40a2cd90f8624b67ef917063
kamalgmbh <kamalgmbh32@gmail.com>
2026-05-02 02:34:31 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone -b scarthgap git://git.yoctoproject.org/poky
+git clone -b scarthgap "git://git.openembedded.org/meta-openembedded"
+git clone https://git.yoctoproject.org/meta-virtualization -b scarthgap
